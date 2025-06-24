21579632c4201f5049fa7df87963cbc6d42c1008
Jose Gutierrez <josearmandog02@gmail.com>
2025-06-24 07:18:50 -0400

    Reassign and redeclare variables class added

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+// Reassign and Redeclare class
+var firstName // Undefined
+firstName = 'Oscar'
+console.log(firstName)
+
+var lastName = 'David' // Declare / assign
+lastName = 'Ana' // Reassign
+console.log(lastName)
+
+var secondName = 'David' // Declare / assign
+var secondName = 'Ana' // Reassign
+console.log(secondName)
+
+// Let
+let fruit = 'Apple' // Declare and assign
+fruit = 'Kiwi' // Reassign
+console.log(fruit)
+
+let fruit = 'Banana'
+console.log(fruit)
+
+// Const
+const animal = 'Dog' // Declare and assign
+// animal = 'Cat' // Reassign
+const animal = 'Snake'
+console.log(animal)
+
+const vehicles = []
+vehicles.push('🚗')
+console.log(vehicles)
+
+vehicles.pop()
+console.log(vehicles)
